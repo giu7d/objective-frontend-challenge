d4a55d9737b496607c4da491b22cfec6350c570c
Giuseppe Setem <giuseppe7d@gmail.com>
2021-01-19 15:26:23 +0000

    fix: limit search input style scope

--- src/components/fragments/SearchInput/styles.ts
+++ src/components/fragments/SearchInput/styles.ts
@@ -5,20 +5,21 @@ export const SearchInputStyled = styled.div`
   flex-direction: column;
   gap: 8px;
 
-  label {
+  & > label {
     font-size: 1.25rem;
     font-weight: bold;
     opacity: 0.75;
   }
 
-  input {
+  & > input {
     padding: 0.5rem 1rem;
     font-size: 1.25rem;
     border-radius: 4px;
     border: 1px solid rgba(0, 0, 0, 0.1);
-  }
-  input::placeholder {
-    font-style: italic;
+
+    ::placeholder {
+      font-style: italic;
+    }
   }
 
   @media screen and (min-width: 600px) {
